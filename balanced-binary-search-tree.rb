e9b4ba6d1e0b03e43e30697bc69f06f51610a41a
Christopher Alagna <christopheralagna1@gmail.com>
2020-09-10 14:18:07 -0700

    completed the balanced binary search tree project

--- balanced-binary-search-tree.rb
+++ balanced-binary-search-tree.rb
@@ -4,27 +4,20 @@ class Node
   include Comparable
   attr_accessor :data, :left, :right
 
-=begin
-  def <=>(other)
-    data <=> other.data
-  end
-=end
-
   def initialize(data)
     @data = data
     @left = nil
     @right = nil
   end
-
 end
 
 class Tree
-  attr_accessor :array, :root, :number_of_nodes
+  attr_accessor :array, :number_of_nodes, :root
 
   def initialize(array)
-    @array = array.sort
+    @array = array.uniq.sort
     @number_of_nodes = 0
-    @root = build_tree(array)
+    @root = build_tree(@array)
   end
 
   def build_tree(array)
@@ -39,60 +32,59 @@ class Tree
     return node
   end
 
-  def insert(value, target_node = @root)
+  def insert(value, root = @root)
     new_node = Node.new(value)
-    return new_node if target_node.nil?
+    return new_node if root.nil?
 
-    case new_node.data <=> target_node.data
+    case new_node.data <=> root.data
     when -1
-      target_node.left = insert(data, target_node.left)
+      root.left = insert(value, root.left)
     when 1
-      target_node.right = insert(data, target_node.right)
+      root.right = insert(value, root.right)
     when 0
       return new_node
     end
-    target_node
+    root
   end
 
-  def delete(value, target_node = @root)
-    node_to_delete = Node.new(value)
-    return target_node if target_node.nil?
+  def delete(value, root = @root)
+    return root if root.nil?
 
-    case node_to_delete.data <=> target_node.data
+    case value <=> root.data
     when -1
-      target_node.left = delete(value, target_node.left)
+      root.left = delete(value, root.left)
     when 1
-      target_node.right = delete(value, target_node.right)
+      root.right = delete(value, root.right)
     when 0
-      if target_node.left == nil
-        temp = target_node.right
-        target_node = nil
+      if root.left == nil
+        temp = root.right
+        root = nil
         return temp
-      elsif target_node.right == nil
-        temp = target_node.left
-        target_node = nil
+      elsif root.right == nil
+        temp = root.left
+        root = nil
         return temp
       else
-        temp = self.inorder(target_node.right)[0]
-        target_node.data = temp
-        target_node.right = delete(temp, target_node.right)
+        temp = self.inorder(root.right)[0]
+        root.data = temp
+        root.right = delete(temp, root.right)
       end
     end
-    target_node
+    root
   end
 
   def find(value)
     nodes = level_order()
-    nodes.each { |node| return node if node.data = value}
+    nodes.each { |node| return node if node.data == value }
   end
 
   def level_order()
     array = []
     queue = Queue.new()
-    queue.push(root)
+    queue.push(@root)
     while !queue.empty?
       node = queue.pop()
-      array << node
+      array << node.data
       queue.push(node.left) if node.left != nil
       queue.push(node.right) if node.right != nil
     end
@@ -123,24 +115,46 @@ class Tree
     return array
   end
 
-  def height()
+  def height(target_node)
+    return -1 if target_node.nil?
 
+    left_height = height(target_node.left)
+    right_height = height(target_node.right)
+
+    left_height > right_height ? left_height + 1 : right_height + 1
   end
 
-  def depth(node, root = @root)
-    depth = 0
-    until root == nil
-      case node.data <=> root.data
-      when -1
-        root = root.left
-        depth += 1
-      when 0
-        return depth
-      when 1
-        root = root.right
-        depth += 1
-      end
+  def depth(target_node, root = @root)
+    return nil if root.nil?
+
+    case target_node.data <=> root.data
+    when -1
+      depth = depth(target_node, root.left)
+      return depth + 1
+    when 1
+      depth = depth(target_node, root.right)
+      return depth + 1
+    when 0
+      return 0
     end
+  end
+
+  def balanced?(root = @root)
+    return -1 if root.nil?
+
+    left_height = height(root.left)
+    right_height = height(root.right)
+    return false if (left_height - right_height).abs() > 1
+    
+    unless left_height == false || right_height == false
+      left_height > right_height ? left_height + 1 : right_height + 1
+    end
+    return true
+  end
+
+  def rebalance()
+    array = self.inorder
+    @root = build_tree(array)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -148,12 +162,24 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
-
 end
 
-array = [1,2,3,4,6,8,9]
-
-new_tree = Tree.new(array)
+new_tree = Tree.new(Array.new(15) { rand(1..100) })
 new_tree.pretty_print
-new_tree.delete(4)
-new_tree.pretty_print
+puts "Is the above tree balanced?: #{new_tree.balanced?()}"
+p "Level order: #{new_tree.level_order()}"
+p "Preorder: #{new_tree.preorder()}"
+p "Postorder: #{new_tree.postorder()}"
+p "Inorder: #{new_tree.inorder()}"
+10.times do
+  new_tree.insert(rand(100..200))
+end
+new_tree.pretty_print()
+puts "Is the above tree balanced?: #{new_tree.balanced?()}"
+new_tree.rebalance()
+new_tree.pretty_print()
+puts "Is the above tree balanced?: #{new_tree.balanced?()}"
+p "Level order: #{new_tree.level_order()}"
+p "Preorder: #{new_tree.preorder()}"
+p "Postorder: #{new_tree.postorder()}"
+p "Inorder: #{new_tree.inorder()}"
